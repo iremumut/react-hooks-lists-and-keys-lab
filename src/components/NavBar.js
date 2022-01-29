3aebd0ba772d366e2978ead3f133ff86cc05ac9e
iremumut <irem2000.nur@gmail.com>
2022-01-29 03:46:57 +0300

    projects added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,14 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linkRender = links.map((link, index) => {
+    return (<a href={"#"+link} key={index}>{link}</a>)
+  })
+
+  return <nav>
+    {/* display an <a> tag for each link here */}
+    {linkRender}
+    </nav>;
 }
 
 export default NavBar;
